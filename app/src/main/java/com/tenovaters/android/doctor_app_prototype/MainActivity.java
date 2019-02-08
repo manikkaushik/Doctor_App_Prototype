0675a854f4d56bd77678853113261b84cb297cca
manikkaushik <manikkaushik99@gmail.com>
2019-02-08 18:59:32 +0530

    Initial commit

---
+++ app/src/main/java/com/tenovaters/android/doctor_app_prototype/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tenovaters.android.doctor_app_prototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
